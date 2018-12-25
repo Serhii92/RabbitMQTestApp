0278a07774392b556952a4e1a17a043da817d65e
DESKTOP-NBA1BI2\Admin <nizhnichenko92@gmail.com>
2018-12-25 22:55:21 +0200

    added producer 3 and 4

---
+++ Producer_Three/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Common;
+using ProducerCommon;
+
+namespace Producer_Three
+{
+	class Program
+	{
+		private static readonly log4net.ILog log =
+			log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		static void Main(string[] args)
+		{
+			var rpcClient = new Producer(
+				Helpers.ResponseQueueName,
+				Helpers.ExchangeName,
+				Helpers.RoutingKey,
+				"Producer THREE");
+
+			Console.WriteLine("Producer THREE");
+			var startTimeSpan = TimeSpan.Zero;
+			var periodTimeSpan = TimeSpan.FromSeconds(5);
+
+			var timer = new System.Threading.Timer((e) =>
+			{
+				Console.WriteLine("Send Message");
+				var response = rpcClient.Call();
+				if (response.ReponseStatus == "FAIL")
+				{
+					log.Info(response);
+				}
+
+				Console.WriteLine(" [.] Got '{0}'", response);
+			}, null, startTimeSpan, periodTimeSpan);
+
+			Console.ReadLine();
+			rpcClient.Close();
+		}
+	}
+}
